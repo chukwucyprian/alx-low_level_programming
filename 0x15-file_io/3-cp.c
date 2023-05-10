ddb1f0fe281568915836b546a6fd3caa4b9e7bbf
chukwucyprian <chukwucyprian262@gmail.com>
2023-05-10 11:51:08 -0700

    3-cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,7 +25,8 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't rd frm file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]), exit(98);
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
 	if (fd_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
@@ -36,7 +37,8 @@ int main(int argc, char **argv)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	if (bytes_rd == -1)
-		dprintf(STDERR_FILENO, "Error: Can't rd frm file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]), exit(98);
 	close_from = close(fd_from);
 	if (close_from == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
