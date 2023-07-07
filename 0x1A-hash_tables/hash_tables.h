c863ed15cca9bab34b7463065a32f7a88b09ce7d
chukwucyprian <chukwucyprian262@gmail.com>
2023-07-07 11:00:47 +0000

    correction update

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -17,9 +17,9 @@
  */
 typedef struct hash_node_s
 {
-    char *key;
-    char *value;
-    struct hash_node_s *next;
+char *key;
+char *value;
+struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -32,8 +32,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-    unsigned long int size;
-    hash_node_t **array;
+unsigned long int size;
+hash_node_t **array;
 } hash_table_t;
 
 /* Function Prototypes */
@@ -43,6 +43,7 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
 
